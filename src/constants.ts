8b535fe2ff3ff115b70e6942e244ebe1a4cf4518
RayHan904 <54216177+RayHan904@users.noreply.github.com>
2024-05-26 15:33:48 -0400

    added https

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string ="http://ec2-3-14-4-255.us-east-2.compute.amazonaws.com:8000"
+export const BASE_URL: string ="https://www.stalcobackend.online:8000"
   // process.env.NODE_ENV === "development"
   //   ? "http://localhost:8000"
   //   : "https://ec2-3-14-4-255.us-east-2.compute.amazonaws.com:8000";
